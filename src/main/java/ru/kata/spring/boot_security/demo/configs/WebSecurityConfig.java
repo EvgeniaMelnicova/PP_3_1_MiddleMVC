999f448d334a5770e96843ce136ece9c28233aa5
Evgenia <Shikme-ai9@mail.ru>
2023-06-16 15:14:31 +0300

    №4. Не проходит авторизация.

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,10 +23,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SuccessUserHandler successUserHandler;
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final Config config;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        // пробуем
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+    // пробуем
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(config.passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
 
     @Override
@@ -36,7 +48,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/admin/**").access("hasAuthority('ADMIN')")
             .antMatchers("/user/**").access("hasAnyAuthority('ADMIN', 'USER')")
             .anyRequest().authenticated()
-            .and().formLogin().loginPage("/login")
+            .and().formLogin()
+               // .loginPage("/login")
             .loginProcessingUrl("/login")
             .usernameParameter("j_email")
             .passwordParameter("j_password")
