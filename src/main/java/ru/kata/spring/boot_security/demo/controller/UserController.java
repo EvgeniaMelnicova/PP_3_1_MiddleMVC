ad081edd415f009d77723b82e846b64baeaf2e43
Evgenia <Shikme-ai9@mail.ru>
2023-06-20 18:20:37 +0300

    20.06.23. Добавлены остальные страницы.

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -18,7 +19,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/user")
-    public String userInfo(Long id, Model model) {
+    public String userInfo(@PathVariable("id") Long id, Model model) {
         Optional<User> user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "user";
