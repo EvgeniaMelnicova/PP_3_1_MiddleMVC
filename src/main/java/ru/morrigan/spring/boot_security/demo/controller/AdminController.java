6fb7e35b23a06377cb26d9c4c61d5a155dc1ab29
Evgenia <Shikme-ai9@mail.ru>
2023-10-15 09:13:34 +0300

    Перепись контроллера.

--- src/main/java/ru/morrigan/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/morrigan/spring/boot_security/demo/controller/AdminController.java
@@ -2,6 +2,7 @@ package ru.morrigan.spring.boot_security.demo.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +20,10 @@ public class AdminController {
     private RoleService roleService;
 
     @GetMapping("/users")
-    public String getUsers(Model model) {
+    public String getAdminPage(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("users", userService.getUsers());
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roleService.getAllRoles());
         return "users";
     }
 
@@ -30,12 +33,18 @@ public class AdminController {
         return "show";
     }
 
+    // Времменный код
+
+    // до сюда
+
     @GetMapping("/users/new")
     public String newUser(@ModelAttribute("user") User user, Model model) {
         model.addAttribute("roles", roleService.getAllRoles());
         return "new";
     }
+    // Времменный код
 
+    // до сюда
     @PostMapping
     public String createUser(@ModelAttribute("user") User user,
                              @RequestParam(value = "roles") String[] roles){
@@ -43,14 +52,18 @@ public class AdminController {
         userService.save(user);
         return "redirect:/admin/users";
     }
+    // Времменный код
 
+    // до сюда
     @GetMapping("/users/{id}/edit")
     public String editUser(Model model, @PathVariable("id") Long id){
         model.addAttribute("roles", roleService.getAllRoles());
         model.addAttribute("user", userService.getUserById(id));
         return "edit";
     }
+    // Времменный код
 
+    // до сюда
     @PatchMapping("/users/{id}")
     public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id,
                              @RequestParam(value = "roles") String[] roles){
@@ -58,10 +71,15 @@ public class AdminController {
         userService.updateUser(user);
         return "redirect:/admin/users";
     }
+    // Времменный код
 
+    // до сюда
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") Long id){
         userService.deleteUser(id);
         return "redirect:/admin/users";
     }
+    // Времменный код
+
+    // до сюда
 }
